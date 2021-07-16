bb97a8da2e01e7cb38b16ea6c62c5e129b6a5d6c
meelosch <57445151+andric-milos@users.noreply.github.com>
2021-07-16 15:04:07 +0200

    Created service class UserService and implemented methods for login and logut
    Maybe in the future, this class should be renamed in KorisnikService, because it's related to class named Korisnik not User.

---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,91 @@
+package services;
+
+import java.io.File;
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import beans.Korisnik;
+import dao.KorisnikDAO;
+
+@Path("/user")
+public class UserService {
+	
+	@Context
+	HttpServletRequest request;
+	
+	@Context
+	ServletContext ctx;
+	
+	public UserService() {
+		
+	}
+	
+	@PostConstruct
+	public void init() {
+		if (ctx.getAttribute("korisnici") == null) {
+			String path = ctx.getRealPath("");
+			ctx.setAttribute("korisnici", new KorisnikDAO(path));
+		}
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(Korisnik k) {
+		Korisnik korisnik = (Korisnik) request.getSession().getAttribute("korisnik");
+		
+		// 1. scenario: korisnik je vec ulogovan
+		if (korisnik != null) {
+			return Response.status(Status.BAD_REQUEST).entity("ALREADY LOGGED IN").build();
+		}
+		
+		// 2. scenario: nevalidan unos podataka
+		if (k.getKorisnickoIme().equals("") || k.getLozinka().equals("")) {
+			return Response.status(Status.BAD_REQUEST).entity("INVALID").build();
+		}
+		
+		KorisnikDAO korisnikDAO = (KorisnikDAO) ctx.getAttribute("korisnici");
+		HashMap<String, Korisnik> korisnici = korisnikDAO.getKorisniciHashMap();
+		
+		// 3. scenario: korisnik sa unetim korisnickim imenom ne postoji
+		if (!korisnici.containsKey(k.getKorisnickoIme()))
+			return Response.status(Status.BAD_REQUEST).entity("USER DOES NOT EXIST").build();
+		
+		korisnik = korisnici.get(k.getKorisnickoIme());	
+		if (korisnik.getLozinka().equals(k.getLozinka())) {
+			// 4. scenario: kredencijali su dobri
+			request.getSession().setAttribute("korisnik", korisnik);
+			return Response.status(Status.OK).build();
+		} else {
+			// 5. scenario: uneto korisnicko ime postoji, ali sifra je pogresna
+			return Response.status(Status.BAD_REQUEST).entity("WRONG PASSWORD").build();
+		}
+	}
+	
+	@POST
+	@Path("/logout")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response logout() {
+		Korisnik korisnik = (Korisnik) request.getSession().getAttribute("korisnik");
+		
+		if (korisnik == null) {
+			return Response.status(Status.BAD_REQUEST).entity("NOT LOGGED IN").build();
+		} else {
+			request.getSession().invalidate();
+			return Response.status(Status.OK).build();
+		}
+	}
+
+}
