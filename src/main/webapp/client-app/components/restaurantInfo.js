82462e2357c1d8daa66887a0986f5c5d1a83d29b
andric-milos <andrinho.num7@gmail.com>
2021-10-20 20:22:02 +0200

    Updated restaurantInfo.js
    Sending two synchronous AJAX calls: Checking if user is logged in, if that is the case, then checking if the user has "manager" authority. If user has proper authority, then getting information about restaurant that he's managing.
    -> Instead of dummy restaurant logo, location and name, now we have real restaurant logo, location and name.

--- src/main/webapp/client-app/components/restaurantInfo.js
+++ src/main/webapp/client-app/components/restaurantInfo.js
@@ -1,18 +1,20 @@
 Vue.component("restaurant-info", {
     data: function() {
         return {
-            
+            showRestaurant: undefined,
+            restaurant: undefined
         }
     },
     template: `
     <div id="page-content-wrapper">
-        <div class="d-flex flex-row justify-content-center">
+        <div v-if="showRestaurant" class="d-flex flex-row justify-content-center">
             <div class="d-flex flex-column p-2">
-                <h1 class="p-2 text-center">Ime restorana</h1>
+                <h1 id="restaurantName" class="p-2 text-center"></h1>
 
                 <div class="d-flex flex-row p-2">
                     <div class="container">
-                        <img src="https://www.alltimelow.com/sites/g/files/g2000006681/f/styles/new_res_custom_user_iphone_portrait_1x/public/Sample-image10-highres.jpg?itok=pHvSQPEY" 
+                        <img 
+                        id="restaurantLogo" 
                         class="border rounded" 
                         height="auto" 
                         width="100%" 
@@ -30,7 +32,11 @@ Vue.component("restaurant-info", {
 
                 <div class="d-flex flex-row justify-content-between p-2">
                     <h3 class="p-2"><b>Articles</b></h3>
-                    <button type="button" class="btn btn-secondary m-2">Add new article</button>
+                    <button 
+                        type="button" 
+                        class="btn btn-secondary m-2" >
+                    Add new article
+                    </button>
                 </div>
                 
                 <div class="d-flex flex-row flex-wrap justify-content-between" style="max-width:570px;">
@@ -96,23 +102,57 @@ Vue.component("restaurant-info", {
                 </div>
             </div>
         </div>
+        <div v-else>
+            <h1 class="p-2 text-center">No restaurant to show.</h1>
+        </div>
     </div> `,
     methods: {
-        
+        initializeMapAndRestaurantInfo() {
+            document.getElementById("restaurantName").innerHTML = this.restaurant.naziv;
+            document.getElementById("restaurantLogo").setAttribute("src", "http://localhost:8080/web-2020-21/images/restaurant-logos/" + this.restaurant.naziv + ".jpg");
+
+            var map = L.map('map').setView([45.2460312, 19.8359505], 13);
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: '',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoibWVlbG9zY2giLCJhIjoiY2trcTRkNzl3MGZ6djJvcW4zeHFxeHg0YyJ9.h06Ayx7JxZqxszi6nLpsZw'
+            }).addTo(map);
+
+            var marker = L.marker([this.restaurant.lokacija.geografskaSirina, this.restaurant.lokacija.geografskaDuzina], {draggable: false});
+            marker.addTo(map);
+        }
     },
     mounted() {
-        var map = L.map('map').setView([45.2460312, 19.8359505], 13);
+        axios.get("rest/user/getLoggedInUserData")
+            .then(response => {
+                if (response.data == "NOT LOGGED IN") {
+                    this.showRestaurant = false;
+                } else {
+                    if (response.data.tipKorisnika == "MENADZER") {
+                        // novi axios poziv
+                        axios.get("rest/restaurant/byManager/" + response.data.korisnickoIme)
+                            .then(response => {
+                                this.restaurant = response.data;
+                                this.showRestaurant = true;
+                                this.initializeMapAndRestaurantInfo();
+                            })
+                            .catch(error => {
+                                console.log(error);
+                                this.showRestaurant = false;
+                            });
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: '',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoibWVlbG9zY2giLCJhIjoiY2trcTRkNzl3MGZ6djJvcW4zeHFxeHg0YyJ9.h06Ayx7JxZqxszi6nLpsZw'
-        }).addTo(map);
-
-        var marker = L.marker([45.2460312, 19.8359505], {draggable: false});
-        marker.addTo(map);
+                        this.showRestaurant = true;
+                    } else {
+                        this.showRestaurant = false;
+                    }
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 });
