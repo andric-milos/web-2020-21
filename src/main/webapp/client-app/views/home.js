48770b821567dfe741cd939b992b55ea1add6858
andric-milos <andrinho.num7@gmail.com>
2021-10-29 00:01:27 +0200

    Implemented method for searching restaurants

--- src/main/webapp/client-app/views/home.js
+++ src/main/webapp/client-app/views/home.js
@@ -1,7 +1,10 @@
 Vue.component('home-page', {
     data: function() {
         return {
-            restaurants: []
+            restaurants: [],
+            restaurantsDisplayed: [],
+            searchInput: undefined,
+            searchBy: "name"
         }
     },
     template: `
@@ -10,7 +13,19 @@ Vue.component('home-page', {
         
         <div class="d-flex p-2 justify-content-center">
             <div class="d-flex flex-column">
-                <div class="d-flex flex-row border border-dark rounded m-2" v-for="r in restaurants">
+                <div class="d-flex flex-row p-2 justify-content-between">
+                    <input type="text" v-model="searchInput" class="p-2" id="searchInput" placeholder="Search">
+                    <div>
+                        <label>Search by:</label>
+                        <select name="searchBy" id="searchBy" v-model="searchBy" style="height: 45px;">
+                            <option value="name">Name</option>
+                            <option value="type">Type</option>
+                            <option value="address">Address</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-row border border-dark rounded m-2" v-for="r in restaurantsDisplayed">
                     <img 
                         v-bind:src="'http://localhost:8080/web-2020-21/images/restaurant-logos/' + r.naziv + '.jpg'" 
                         class="border-end border-dark rounded p-2" 
@@ -32,6 +47,7 @@ Vue.component('home-page', {
             .then(response => {
                 if (response.status == 200) {
                     this.restaurants = response.data;
+                    this.restaurantsDisplayed = response.data;
                 } else {
                     console.log(response);
                 }
@@ -39,5 +55,34 @@ Vue.component('home-page', {
             .catch(error => {
                 console.log(error);
             });
+    },
+    watch: {
+        searchInput: function(value) {
+            if (value == "") {
+                this.restaurantsDisplayed = this.restaurants;
+            } else {
+                this.restaurantsDisplayed = [];
+
+                if (this.searchBy == "name") {
+                    for (let i = 0; i < this.restaurants.length; i++) {
+                        if (this.restaurants[i].naziv.toLowerCase().startsWith(value.toLowerCase())) {
+                            this.restaurantsDisplayed.push(this.restaurants[i]);
+                        }
+                    }
+                } else if (this.searchBy == "type") {
+                    for (let i = 0; i < this.restaurants.length; i++) {
+                        if (this.restaurants[i].tip.toLowerCase().startsWith(value.toLowerCase())) {
+                            this.restaurantsDisplayed.push(this.restaurants[i]);
+                        }
+                    }
+                } else if (this.searchBy == "address") {
+                    for (let i = 0; i < this.restaurants.length; i++) {
+                        if (this.restaurants[i].lokacija.adresa.mesto.toLowerCase().startsWith(value.toLowerCase())) {
+                            this.restaurantsDisplayed.push(this.restaurants[i]);
+                        }
+                    }
+                }
+            }
+        }
     }
 });
